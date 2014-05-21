a783e3d23fd8a46010033a73cd1c90f835001ab4
Anderson Aguiar <andersonaguiarrr@gmail.com>
2014-05-20 21:14:11 -0300

    chore(dev.js): clean unnecessary task and modify require to single line

--- tasks/dev.js
+++ tasks/dev.js
@@ -2,16 +2,7 @@ function loadModule(module) {
     global[module.replace(/^gulp-/, '')] = require(module);
 }
 
-require('matchdep')
-    .filterDev('gulp*')
-    .forEach(loadModule);
-
-// Get one .styl file and render
-gulp.task('one', function () {
-    gulp.src('assets/stylus/main.styl')
-    .pipe(stylus())
-    .pipe(gulp.dest('assets/css'));
-});
+require('matchdep').filterDev('gulp*').forEach(loadModule);
 
 // Get and render all .styl files recursively
 gulp.task('stylus', function () {
