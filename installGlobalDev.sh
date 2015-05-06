ac51d6a400c88a04e63c5ff208239772887c792d
Anderson Aguiar <revsoft10@gmail.com>
2015-05-05 23:29:06 -0300

    Add conditions to sh file

--- installGlobalDev.sh
+++ installGlobalDev.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-sudo npm i -g mocha;
-sudo npm i -g gulp;
-sudo npm i -g browser-sync;
-sudo npm i -g esformatter;
-sudo gem install compass;
+type gulp >/dev/null 2>&1 || {
+  sudo npm i -g gulp;
+}
+
+type browser-sync >/dev/null 2>&1 || {
+  sudo npm i -g browser-sync;
+}
+
+type esformatter >/dev/null 2>&1 || {
+  sudo npm i -g esformatter;
+}
+
+type compass >/dev/null 2>&1 || {
+  sudo gem install compass;
+}
+
+type scss-lint >/dev/null 2>&1 || {
+  sudo gem install scss-lint;
+}
