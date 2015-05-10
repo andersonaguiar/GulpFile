69d7042d16f5b893a824eda38db3c54e6e22a1ce
Anderson Aguiar <revsoft10@gmail.com>
2015-05-10 20:26:43 -0300

    improve requires

--- gulp/config.js
+++ gulp/config.js
@@ -2,67 +2,67 @@
 
 // =======================  LOAD PLUGINS ====================================
 
-global['gulp']    = require('gulp');
-global['$']       = require('gulp-load-plugins')({
-                        pattern       : ['*']
-                      , scope         : ['dependencies', 'devDependencies']
-                      , replaceString : /^gulp(-|\.)/
-                      , camelize      : true
-                      , lazy          : true
-                      , rename        : {
-                          'gulp-filesize': 'size'
-                      }
-                    });
+global['gulp']  = require('gulp');
+global['$']     = require('gulp-load-plugins')({
+                    pattern       : ['*'],
+                    scope         : ['dependencies', 'devDependencies'],
+                    replaceString : /^gulp(-|\.)/,
+                    camelize      : true,
+                    lazy          : true,
+                    rename        : {
+                      'gulp-filesize': 'size'
+                    }
+                  });
 
 // ==========================  CONFIGS ======================================
 
 // paths map
 var path = {
-    src:  'src'
-  , dest: 'build'
+  src:  'src',
+  dest: 'build'
 };
 
 // set configs for plugins
-module.exports = {
-  src: path.src,
+global['config'] = module.exports = {
+  src:  path.src,
   dest: path.dest,
   tests: {
     src: path.src + '/js/tests/**/*'
   },
   zip: {
     name: 'build.zip',
-    src: path.dest + '/**/*',
+    src:  path.dest + '/**/*',
     dest: './'
   },
   esformatter: {
-    src: path.src + '/js/*',
+    src:  path.src + '/js/*',
     dest: path.src + '/js'
   },
   js: {
-    src: path.src + '/js/*.js',
+    src:  path.src + '/js/*.js',
     dest: path.dest + '/assets/js'
   },
   browserSync: {
     server: { baseDir: path.dest }
   },
   images: {
-    src: path.src + '/images/**',
+    src:  path.src + '/images/**',
     dest: path.dest + '/assets/images'
   },
   markup: {
-    src: path.src + '/**/*.html',
+    src:  path.src + '/**/*.html',
     dest: path.dest
   },
   compass: {
-    src: path.src + '/sass',
+    src:  path.src + '/sass',
     dest: path.dest + '/assets/css'
   },
   scssLint: {
     conf: path.src + '/../scss-lint.yml'
   },
   preprocessor: {
-    choice    : 'sass', //sass or stylus
-    src       : path.src + '/sass/**/*.scss', //dir of sass or stylus
-    dest      : path.dest + '/assets/css' //dir of css dest
+    choice: 'sass', //sass or stylus
+    src:    path.src + '/sass/**/*.scss', //dir of sass or stylus
+    dest:   path.dest + '/assets/css' //dir of css dest
   }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 // Require all tasks in gulp/tasks, including subfolders
+var config      = require('./gulp/config');
 var requireDir  = require('require-dir')('./gulp/tasks', { recurse: true });
 
 
